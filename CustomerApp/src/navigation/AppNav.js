8dc7de2eeae7e36ed0298000446cc392093563a7
Vy <vy.do@pharmapoc.com>
2023-07-10 22:40:59 +0700

    chore: setup tailwind

--- CustomerApp/src/navigation/AppNav.js
+++ CustomerApp/src/navigation/AppNav.js
@@ -1,19 +1,18 @@
-import {View, Text, ActivityIndicator} from 'react-native';
 import React, {useContext} from 'react';
+import {ActivityIndicator, View} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import AuthStack from './AuthStack';
 import {AuthContext} from '../context/AuthContext';
 import HomeScreen from '../screens/HomeScreen';
+import AuthStack from './AuthStack';
 
 const AppNav = () => {
   const {isLoading, userToken} = useContext(AuthContext);
 
   if (isLoading) {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View className="flex-1 items-center justify-center bg-red-500">
         <ActivityIndicator size={'large'} />
       </View>
     );
